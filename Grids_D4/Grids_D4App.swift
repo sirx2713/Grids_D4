09acc3e179c570ea9200c7601e634cfcbf47d711
sirx2713 <bradx2713@gmail.com>
2024-03-16 08:32:25 +0300

    Initial Commit

---
+++ Grids_D4/Grids_D4App.swift
@@ -0,0 +1,17 @@
+//
+//  Grids_D4App.swift
+//  Grids_D4
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/16.
+//
+
+import SwiftUI
+
+@main
+struct Grids_D4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
